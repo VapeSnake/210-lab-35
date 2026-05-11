d40efb2513c53cdbcf049831955cfecef8ea9011
Noel Mier-Luna <noel.lmier@gmail.com>
2026-05-11 14:35:17 -0700

    4th Commit

--- 210-lab-35.cpp
+++ 210-lab-35.cpp
@@ -26,9 +26,70 @@ int main()
     // Display the tree contents using In-order traversal so strings are displayed in non-decreasing order. I prefer saying increasing order.
     cout << "In-order Traversal:" << endl;
     tree.displayInOrder();
-    
 
+    // Main menu that allows user to add, search for, or delete a node in the tree.
+    int choice;
+    do
+    {
+        cout << "\nMain Menu:" << endl;
+        cout << "1. Add a code" << endl;
+        cout << "2. Search for a code" << endl;
+        cout << "3. Delete a code" << endl;
+        cout << "4. Display the tree" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
+        switch (choice)
+        {
+        case 1:
+            // Add a code
+            cout << "Enter a code to add: ";
+            cin >> code; // Allows user to input a code to add to the tree
+            tree.insertNode(code);
+            cout << code << " added to the tree." << endl;
+
+            break;
+        case 2:
+            // Search for a code
+            cout << "Enter a code to search for: ";
+            cin >> code;
+            if (tree.searchNode(code)) // Check if code exists in the tree
+            {
+                cout << code << " found in the tree." << endl;
+            }
+            else
+            {
+                cout << code << " not found in the tree." << endl;
+            }
+            break;
+        case 3:
+            // Delete a code
+            cout << "Enter a code to delete: ";
+            cin >> code;
+            if (tree.searchNode(code)) // Check if code exists before attempting to delete
+            {
+                tree.remove(code);
+                cout << code << " deleted from the tree." << endl;
+            }
+            else
+            {
+                cout << code << " not found in the tree." << endl;
+            }
+            break;
+        case 4:
+            // Display the tree
+            cout << "Tree contents:" << endl;
+            tree.displayInOrder();
+            break;
+        case 5:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl; // Handle invalid menu choices
+            break;
+        }
+    } while (choice != 5);
 
     return 0;
 }
