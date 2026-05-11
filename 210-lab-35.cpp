7b3e562b252ea273d4db6c74ed3653f21503a91d
Noel Mier-Luna <noel.lmier@gmail.com>
2026-05-11 14:28:31 -0700

    Milestone 3

--- 210-lab-35.cpp
+++ 210-lab-35.cpp
@@ -23,39 +23,11 @@ int main()
     }
     inFile.close(); // Close the file after reading
 
-    cout << "Preorder traversal of the tree:" << endl;
-    tree.displayPreOrder(); // Display the tree in preorder
-
-    cout << "\nInorder traversal of the tree:" << endl;
-    tree.displayInOrder(); // Display the tree in inorder
-
-    // Test searchNode function
-    string searchCode = "HotDog";
-    if (tree.searchNode(searchCode))
-    {
-        cout << "\nCode found in the tree." << endl; // Notifies if code/string is found in tree, otherwise says not found.
-    }
-    else
-    {
-        cout << "\nCode not found in the tree." << endl;
-    }
-
-    // Test remove function
-    string removeCode = "zzzvpPa2";
+    // Display the tree contents using In-order traversal so strings are displayed in non-decreasing order. I prefer saying increasing order.
+    cout << "In-order Traversal:" << endl;
+    tree.displayInOrder();
     
-    tree.remove(removeCode); // Remove the specified code from the tree
-    cout << "\nInorder traversal after removing " << removeCode << ":" << endl;
-    tree.displayInOrder(); // Display the tree in inorder after removal
 
-    // Test Insert.
-    string newCode = "zzzzNewCode123";
-    tree.insertNode(newCode); // Insert the new code into the tree
-    cout << "\nInorder traversal after inserting " << newCode << ":" << endl;
-    tree.displayInOrder(); // Display the tree in inorder after insertion
-
-    tree.displayPostOrder(); // Display the tree in postorder
-
-    // Features confirmed working: insertNode, searchNode, remove, displayInOrder, displayPreOrder and displayPostOrder.
 
 
     return 0;
